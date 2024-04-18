0a9c99e3fee07423dd1612d9e837d19757d61868
Jeri Glicksman <jglicksman3@gatech.edu>
2024-04-17 22:24:54 -0500

    update raw pkl/preprocess w audio feats

--- TL-ERC/iemocap_preprocess.py
+++ TL-ERC/iemocap_preprocess.py
@@ -17,45 +17,51 @@ iemocap_dir = datasets_dir.joinpath('iemocap/')
 iemocap_pickle = iemocap_dir.joinpath("IEMOCAP_features_raw.pkl")
 GLOVE_DIR = project_dir.joinpath('glove/')
 
-# Tokenizer
+# Text Tokenizer
 tokenizer = Tokenizer('spacy')
+
 
 class IEMOCAP:
     '''
     label index mapping = {'hap':0, 'sad':1, 'neu':2, 'ang':3, 'exc':4, 'fru':5}
     '''
     def load_iemocap_data(self):
-        (sent_ids,
+        (self.sent_ids,
          self.videoSpeakers,
          self.videoLabels,
-         text_data,
+         self.text_data,
          self.videoAudio,
          self.videoVisual,
          self.videoSentence,
-         trainVid,
+         self.audioWav2vec,
+         self.trainVid_raw,
          self.testVid) = pickle.load(open(iemocap_pickle, "rb"), encoding="latin1")
 
         self.trainVid, self.valVid = train_test_split(
-            list(trainVid), test_size=.2, random_state=1227)
+            list(self.trainVid_raw), test_size=.2, random_state=1227)
         
         self.vids = {"train":self.trainVid, "valid":self.valVid, "test":self.testVid}
         
         # Calculating maximum sentence length
         self.max_conv_length = max([len(self.videoSentence[vid]) for vid in self.trainVid])
-        
+
 
 def tokenize_conversation(lines):
     sentence_list = [tokenizer(line) for line in lines]
     return sentence_list
 
 
-def pad_sentences(conversations, max_sentence_length=30, max_conversation_length=10):
+def pad_sentences(conversations,
+                  max_sentence_length=30,
+                  max_conversation_length=10,
+                  eos_token=EOS_TOKEN,
+                  pad_token=PAD_TOKEN):
     def pad_tokens(tokens, max_sentence_length=max_sentence_length):
         n_valid_tokens = len(tokens)
         if n_valid_tokens > max_sentence_length - 1:
             tokens = tokens[:max_sentence_length - 1]
         n_pad = max_sentence_length - n_valid_tokens - 1
-        tokens = tokens + [EOS_TOKEN] + [PAD_TOKEN] * n_pad
+        tokens = list(tokens) + [eos_token] + [pad_token] * n_pad
         return tokens
 
     def pad_conversation(conversation):
@@ -68,8 +74,7 @@ def pad_sentences(conversations, max_sentence_length=30, max_conversation_length
     for conversation in conversations:
         if len(conversation) > max_conversation_length:
             conversation = conversation[:max_conversation_length]
-        sentence_length = [min(len(sentence) + 1, max_sentence_length)  # +1 for EOS token
-                           for sentence in conversation]
+        sentence_length = [min(len(sentence) + 1, max_sentence_length) for sentence in conversation]
         all_sentence_length.append(sentence_length)
 
         sentences = pad_conversation(conversation)
@@ -97,7 +102,7 @@ if __name__ == '__main__':
     parser.add_argument('--max_vocab_size', type=int, default=20000)
     parser.add_argument('--min_vocab_frequency', type=int, default=5)
 
-    args = parser.parse_args()
+    args = parser.parse_known_args()[0]
 
     max_sent_len = args.max_sentence_length
     max_conv_len = iemocap.max_conv_length
@@ -112,16 +117,18 @@ if __name__ == '__main__':
 
         conv_sentences = [iemocap.videoSentence[vid] for vid in iemocap.vids[split_type]]
         conv_labels = [iemocap.videoLabels[vid] for vid in iemocap.vids[split_type]]
-        conv_audio = [iemocap.videoAudio[vid] for vid in iemocap.vids[split_type]]
         conv_visual = [iemocap.videoVisual[vid] for vid in iemocap.vids[split_type]]
+
+        conv_audio = [iemocap.videoAudio[vid] for vid in iemocap.vids[split_type]]
+        conv_audio_wav2vec = [iemocap.audioWav2vec[vid] for vid in iemocap.vids[split_type]]
+
 
         print(f'Processing {split_type} dataset...')
         split_data_dir = iemocap_dir.joinpath(split_type)
         split_data_dir.mkdir(exist_ok=True)
         
         conv_sentences = list([tokenize_conversation(conv) for conv in conv_sentences])
-        conversation_length = [min(len(conv), max_conv_len)
-                               for conv in conv_sentences]
+        conversation_length = [min(len(conv), max_conv_len) for conv in conv_sentences]
 
         # fix labels as per conversation_length
         for idx, conv_len in enumerate(conversation_length):
@@ -132,13 +139,16 @@ if __name__ == '__main__':
             max_sentence_length=max_sent_len,
             max_conversation_length=max_conv_len)
 
+        #truncate audio data to keep memory under control
+        # trunc_raw_audio = []
+        # for conv in conv_audio_raw:
+        #     trunc_raw_audio.append([sent[:max_audio_len] for sent in conv])
+
         for sentence_len, label in zip(conversation_length, conv_labels):
             assert(sentence_len ==len(label))
 
-        
         print('Saving preprocessed data at', split_data_dir)
-        to_pickle(conversation_length, split_data_dir.joinpath(
-            'conversation_length.pkl'))
+        to_pickle(conversation_length, split_data_dir.joinpath('conversation_length.pkl'))
         to_pickle(sentences, split_data_dir.joinpath('sentences.pkl'))
         to_pickle(conv_labels, split_data_dir.joinpath('labels.pkl'))
         to_pickle(sentence_length, split_data_dir.joinpath('sentence_length.pkl'))
@@ -147,6 +157,7 @@ if __name__ == '__main__':
         #currently no procesing/padding being done here
         to_pickle(conv_visual, split_data_dir.joinpath('visuals.pkl'))
         to_pickle(conv_audio, split_data_dir.joinpath('audio.pkl'))
+        to_pickle(conv_audio_wav2vec, split_data_dir.joinpath('audio_wav2vec.pkl'))
 
         if split_type == 'train':
             print('Save Vocabulary...')
