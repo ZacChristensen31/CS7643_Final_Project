77dd0e83c6b804e3f32116eb0edfe46c9727c692
Jeri Glicksman <Jeri.Glicksman@nisa.com>
2024-04-22 21:17:20 -0500

    run multi config

--- TL-ERC/bert_model/train.py
+++ TL-ERC/bert_model/train.py
@@ -22,86 +22,102 @@ def summarize_run(solver):
 
 if __name__ == '__main__':
 
-    config = get_config(mode='train',parse=False, modalities=['audio'], run_name='TEST_AUDIO')
+
+    # multi = get_config(mode='train',parse=False, modalities=['combined'], run_name='TEST_COMBO',
+    #                    visual_checkpoint = r"C:\Users\jglicksm\git\CS7643_Final_Project\TL-ERC\run_outputs\TEST_VISUAL\2024-04-22_17-57-21_Run2\best_model.pth",
+    #                    audio_checkpoint = r"C:\Users\jglicksm\git\CS7643_Final_Project\TL-ERC\run_outputs\HIGHER_LR_.001\2024-04-22_11-49-03_Run2\best_model.pth",
+    #                    text_checkpoint = r"C:\Users\jglicksm\git\CS7643_Final_Project\TL-ERC\run_outputs\PRETRAINED_TEXT\2024-04-22_18-36-44_Run0\best_model.pth"
+    #                    )
+    text = get_config(mode='train',parse=False, modalities=['text'], run_name='PRETRAINED_TEXT', n_epochs=15)
+    audio_lstm = get_config(mode='train',parse=False, modalities=['audio'], run_name='AUDIO_LSTM', audio_rnn='LSTM')
+
+    configs=[text, audio_lstm]
     val_config = get_config(mode='valid',parse=False,)
     test_config = get_config(mode='test',parse=False)
 
-    _RUNS = 3
-    summary = {}
-    for run in range(_RUNS):
-        print(config)
-        # No. of videos to consider
-        training_data_len = int(config.training_percentage * \
-            len(load_pickle(config.sentences_path)))
+    _RUNS = 10
+    for config in configs:
+        summary = {}
+        for run in range(_RUNS):
+            print(config)
+            # No. of videos to consider
+            training_data_len = int(config.training_percentage * \
+                len(load_pickle(config.sentences_path)))
 
-        train_data_loader = get_loader(
-            sentences=load_pickle(config.sentences_path)[:training_data_len],
-            labels=load_pickle(config.label_path)[:training_data_len],
-            conversation_length=load_pickle(config.conversation_length_path)[:training_data_len],
-            sentence_length=load_pickle(config.sentence_length_path)[:training_data_len],
-            audio=load_pickle(config.audio_path)[:training_data_len],
-            audioRaw=load_pickle(config.audioRaw_path)[:training_data_len],
-            visual=load_pickle(config.visual_path)[:training_data_len],
-            batch_size=config.batch_size)
+            train_data_loader = get_loader(
+                sentences=load_pickle(config.sentences_path)[:training_data_len],
+                labels=load_pickle(config.label_path)[:training_data_len],
+                conversation_length=load_pickle(config.conversation_length_path)[:training_data_len],
+                sentence_length=load_pickle(config.sentence_length_path)[:training_data_len],
+                audio=load_pickle(config.audio_path)[:training_data_len],
+                audioRaw=load_pickle(config.audioRaw_path)[:training_data_len],
+                visual=load_pickle(config.visual_path)[:training_data_len],
+                batch_size=config.batch_size)
 
-        eval_data_loader = get_loader(
-            sentences=load_pickle(val_config.sentences_path),
-            labels=load_pickle(val_config.label_path),
-            conversation_length=load_pickle(val_config.conversation_length_path),
-            sentence_length=load_pickle(val_config.sentence_length_path),
-            audio=load_pickle(val_config.audio_path),
-            audioRaw=load_pickle(val_config.audioRaw_path),
-            visual=load_pickle(val_config.visual_path),
-            batch_size=val_config.eval_batch_size,
-            shuffle=False)
+            eval_data_loader = get_loader(
+                sentences=load_pickle(val_config.sentences_path),
+                labels=load_pickle(val_config.label_path),
+                conversation_length=load_pickle(val_config.conversation_length_path),
+                sentence_length=load_pickle(val_config.sentence_length_path),
+                audio=load_pickle(val_config.audio_path),
+                audioRaw=load_pickle(val_config.audioRaw_path),
+                visual=load_pickle(val_config.visual_path),
+                batch_size=val_config.eval_batch_size,
+                shuffle=False)
 
-        #commenting out for now to save memory since we arent using
-        # test_data_loader = get_loader(
-        #     sentences=load_pickle(test_config.sentences_path),
-        #     labels=load_pickle(test_config.label_path),
-        #     conversation_length=load_pickle(test_config.conversation_length_path),
-        #     sentence_length=load_pickle(test_config.sentence_length_path),
-        #     audio=load_pickle(test_config.audio_path),
-        #     audioRaw=load_pickle(test_config.audioRaw_path),
-        #     visual=load_pickle(test_config.visual_path),
-        #     batch_size=test_config.eval_batch_size,
-        #     shuffle=False)
+            #commenting out for now to save memory since we arent using
+            # test_data_loader = get_loader(
+            #     sentences=load_pickle(test_config.sentences_path),
+            #     labels=load_pickle(test_config.label_path),
+            #     conversation_length=load_pickle(test_config.conversation_length_path),
+            #     sentence_length=load_pickle(test_config.sentence_length_path),
+            #     audio=load_pickle(test_config.audio_path),
+            #     audioRaw=load_pickle(test_config.audioRaw_path),
+            #     visual=load_pickle(test_config.visual_path),
+            #     batch_size=test_config.eval_batch_size,
+            #     shuffle=False)
 
-        # for testing
-        solver = Solver(config,
-                        train_data_loader,
-                        eval_data_loader,
-                        test_data_loader=None,
-                        is_train=True,
-                        models=[])
+            # for testing
+            solver = Solver(config,
+                            train_data_loader,
+                            eval_data_loader,
+                            test_data_loader=None,
+                            is_train=True,
+                            models=[])
 
-        outputs = '../run_outputs'
-        if config.run_name is not None:
-            outputs += f"/{config.run_name}/"
+            outputs = '../run_outputs'
+            if config.run_name is not None:
+                outputs += f"/{config.run_name}/"
 
-        if not os.path.exists(outputs):
-            os.makedirs(outputs)
+            if not os.path.exists(outputs):
+                os.makedirs(outputs)
 
-        #append seed/run number
-        run_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        run_directory = f'{outputs}/{run_timestamp}_Run{run}'
+            #append seed/run number
+            run_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            run_directory = f'{outputs}/{run_timestamp}_Run{run}'
 
-        if not os.path.exists(run_directory):
-            os.makedirs(run_directory)
+            if not os.path.exists(run_directory):
+                os.makedirs(run_directory)
 
-        # Save Configs for train, test, valid
-        with open(f'{run_directory}/train_config.txt', 'w') as f:
-            f.write(str(config))
+            # Save Configs for train, test, valid
+            with open(f'{run_directory}/train_config.txt', 'w') as f:
+                f.write(str(config))
 
-        with open(f'{run_directory}/valid_config.txt', 'w') as f:
-            f.write(str(val_config))
+            with open(f'{run_directory}/valid_config.txt', 'w') as f:
+                f.write(str(val_config))
 
-        with open(f'{run_directory}/test_config.txt', 'w') as f:
-            f.write(str(test_config))
+            with open(f'{run_directory}/test_config.txt', 'w') as f:
+                f.write(str(test_config))
 
-        solver.build()
-        solver.train(run_directory=run_directory)
-        summary[run] = pd.concat(summarize_run(solver))
+            solver.build()
+            solver.train(run_directory=run_directory)
+            summary[run] = pd.concat(summarize_run(solver))
 
-    summary = pd.DataFrame(summary, index=['BestEpoch','BestTrainLoss','BestValLoss','BestTrainF1','BestValF1'])
-    summary.T.to_csv(outputs+"\\summary.csv")
+        #save out summary across runs
+        summary = pd.concat(summary,axis=1).T
+        summary.columns = ['BestEpoch','BestTrainLoss','BestValLoss','BestTrainF1','BestValF1']
+        summary.loc['AVG'] = summary.mean(axis=0)
+        summary.loc['MED'] = summary.median(axis=0)
+        summary.loc['MAX'] = summary.max(axis=0)
+        summary.loc['MIN'] = summary.min(axis=0)
+        summary.to_csv(outputs+"/summary.csv")
